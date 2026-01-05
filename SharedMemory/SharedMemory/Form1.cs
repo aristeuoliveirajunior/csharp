c062e0f522b7c011e4de94c6778d40496ae64482
Aristeu <aristeu@celebre.com.br>
2026-01-05 08:49:55 -0300

    criação MemoryMappedFile

--- SharedMemory/SharedMemory/Form1.cs
+++ SharedMemory/SharedMemory/Form1.cs
@@ -7,80 +7,104 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.MemoryMappedFiles;
 
 namespace SharedMemory
 {
     public partial class Form1 : Form
     {
-        private string[,] posicoes = new string[3,3];
+        MemoryMappedFile posicoes;
+
         public Form1()
         {
             InitializeComponent();
+
+             long tamanho = 9 * sizeof(char);
+
+             posicoes = MemoryMappedFile.CreateOrOpen("sharedMemory", tamanho);
         }
 
-        private string validaValor(string valor)
+        private char validaValor(char valor)
         {
-            if (string.IsNullOrEmpty(valor))
-                return "X";
-            else if (valor == "X")
-                return "O";
+            if (valor== '\0')
+                return 'X';
+            else if (valor == 'X')
+                return 'O';
             else
-                return "";
+                return '\0';
         }
+
+        private char getValor(int linha, int coluna)
+        {
+            var access = posicoes.CreateViewAccessor();
+
+            int posicao= linha * coluna * sizeof(char);
+
+            return access.ReadChar(posicao);
+
+        }
+
         private void btn1_Click(object sender, EventArgs e)
         {
-           
-            posicoes[0, 0] = validaValor(posicoes[0, 0]);
-            btn1.Text = posicoes[0, 0];
+            var access = posicoes.CreateViewAccessor();
+
+            int linha = 0;
+            int coluna = 0;
+            int posicao = linha * coluna * sizeof(char);
+
+            access.ReadChar(posicao);
+
+            posicoes[0, 0] = validaValor(getValor(0,0));
+            btn1.Text = posicoes[0, 0].ToString();
 
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
-            posicoes[0, 1] = validaValor(posicoes[0, 1]);
-            btn2.Text = posicoes[0, 1];
+            posicoes[0, 1] = validaValor(getValor(0, 1));
+            btn2.Text = posicoes[0, 1].ToString();
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
-            posicoes[0, 2] = validaValor(posicoes[0, 2]);
-            btn3.Text = posicoes[0, 2];
+            posicoes[0, 2] = validaValor(getValor(0, 2));
+            btn3.Text = posicoes[0, 2].ToString();
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
-            posicoes[1, 0] = validaValor(posicoes[1, 0]);
-            btn4.Text = posicoes[1, 0];
+            posicoes[1, 0] = validaValor(getValor(1, 0));
+            btn4.Text = posicoes[1, 0].ToString();
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
-            posicoes[1, 1] = validaValor(posicoes[1, 1]);
-            btn5.Text = posicoes[1, 1];
+            posicoes[1, 1] = validaValor(getValor(1, 1));
+            btn5.Text = posicoes[1, 1].ToString();
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
-            posicoes[1, 2] = validaValor(posicoes[1, 2]);
-            btn6.Text = posicoes[1, 2];
+            posicoes[1, 2] = validaValor(getValor(1, 2));
+            btn6.Text = posicoes[1, 2].ToString();
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
-            posicoes[2, 0] = validaValor(posicoes[2, 0]);
-            btn7.Text = posicoes[2, 0];
+            posicoes[2, 0] = validaValor(getValor(2, 0));
+            btn7.Text = posicoes[2, 0].ToString();
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
-            posicoes[2, 1] = validaValor(posicoes[2, 1]);
-            btn8.Text = posicoes[2,1];
+            posicoes[2, 1] = validaValor(getValor(2, 1));
+            btn8.Text = posicoes[2,1].ToString();
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
-            posicoes[2, 2] = validaValor(posicoes[2, 2]);
-            btn9.Text = posicoes[2, 2];
+            posicoes[2, 2] = validaValor(getValor(2, 2));
+            btn9.Text = posicoes[2, 2].ToString();
         }
     }
 }
