7ce8b95699f7c1c0957bbdef6455a5b163c4082b
Aristeu <aristeu@celebre.com.br>
2026-01-25 09:35:55 -0300

    copy from shared version to initiate the development version using named pipes

---
+++ NamedPipes/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace NamedPipes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.chkCirculo = new System.Windows.Forms.CheckBox();
+            this.chkLetraX = new System.Windows.Forms.CheckBox();
+            this.bntReiniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(198, 98);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(177, 97);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(372, 98);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(177, 97);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(541, 98);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(177, 97);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(198, 190);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(177, 97);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(372, 190);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(177, 97);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(541, 190);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(177, 97);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(198, 283);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(177, 97);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(372, 283);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(177, 97);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(541, 283);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(177, 97);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // chkCirculo
+            // 
+            this.chkCirculo.AutoSize = true;
+            this.chkCirculo.Location = new System.Drawing.Point(629, 427);
+            this.chkCirculo.Name = "chkCirculo";
+            this.chkCirculo.Size = new System.Drawing.Size(70, 20);
+            this.chkCirculo.TabIndex = 9;
+            this.chkCirculo.Text = "Círculo";
+            this.chkCirculo.UseVisualStyleBackColor = true;
+            this.chkCirculo.CheckedChanged += new System.EventHandler(this.chkCirculo_CheckedChanged);
+            // 
+            // chkLetraX
+            // 
+            this.chkLetraX.AutoSize = true;
+            this.chkLetraX.Location = new System.Drawing.Point(629, 463);
+            this.chkLetraX.Name = "chkLetraX";
+            this.chkLetraX.Size = new System.Drawing.Size(70, 20);
+            this.chkLetraX.TabIndex = 10;
+            this.chkLetraX.Text = "Letra X";
+            this.chkLetraX.UseVisualStyleBackColor = true;
+            this.chkLetraX.CheckedChanged += new System.EventHandler(this.chkLetraX_CheckedChanged);
+            // 
+            // bntReiniciar
+            // 
+            this.bntReiniciar.Location = new System.Drawing.Point(198, 404);
+            this.bntReiniciar.Name = "bntReiniciar";
+            this.bntReiniciar.Size = new System.Drawing.Size(162, 64);
+            this.bntReiniciar.TabIndex = 11;
+            this.bntReiniciar.Text = "Reiniciar";
+            this.bntReiniciar.UseVisualStyleBackColor = true;
+            this.bntReiniciar.Click += new System.EventHandler(this.bntReiniciar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 532);
+            this.Controls.Add(this.bntReiniciar);
+            this.Controls.Add(this.chkLetraX);
+            this.Controls.Add(this.chkCirculo);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Jogo da Velha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.CheckBox chkCirculo;
+        private System.Windows.Forms.CheckBox chkLetraX;
+        private System.Windows.Forms.Button bntReiniciar;
+    }
+}
+
